8da77db81070873d6bb2a9ba618756d6171b8f00
asnow73 <albertulgu@list.ru>
2014-05-20 18:29:43 +0000

    Good view for sign in/out and face

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,12 @@ require 'my_app_settings'
 describe "StaticPages" do
   describe "home page" do
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title(MyAppSettings.title)
     end
 
     it "should have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title('| Home')
     end
   end
